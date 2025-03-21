f3651126d7a9ec82a0c2ed593e069b45925229a5
Zanderbolt-94 <csolutions.tecno@gmail.com>
2025-03-21 13:32:19 -0600

    Inicio de Proyecto Next.JS

---
+++ my-app/src/app/page.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Home() {
+  return (
+    <div>
+      <h1 className="text-white bg-black h-screen flex items-center justify-center p-2 text-2xl">
+        This is Home
+      </h1>
+    </div>
+  );
+}
+
+export default Home;
